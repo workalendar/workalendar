2e1b7bb6e44464dd51e6ac82152cba15a91b3065
Grigouze <grigouze@yahoo.fr>
2013-12-03 20:51:44 +0100

    add calculation of equinoxes with pyephem

--- workalendar/asia.py
+++ workalendar/asia.py
@@ -39,12 +39,10 @@ class JapanCalendar(WesternCalendar):
 
     FIXED_HOLIDAYS = WesternCalendar.FIXED_HOLIDAYS + (
         (2, 11, "Foundation Day"),
-        (3, 20, "Vernal Equinox Day"),  # it can change +1 day
         (4, 29, "Shōwa Day"),
         (5, 3, "Constitution Memorial Day"),
         (5, 4, "Greenery Day"),
         (5, 5, "Children's Day"),
-        (9, 23, "Autumnal Equinox Day"),  # it can change +1 day
         (11, 3, "Culture Day"),
         (11, 23, "Labour Thanksgiving Day"),
         (12, 23, "The Emperor's Birthday"),
@@ -52,6 +50,9 @@ class JapanCalendar(WesternCalendar):
 
     def get_variable_days(self, year):
         # usual variable days
+
+        equinoxes = self.calculate_equinoxes(year)
+
         days = super(JapanCalendar, self).get_variable_days(year)
         days += [
             (WesternCalendar.get_nth_weekday_in_month(year, 1, MON, 2),
@@ -60,11 +61,33 @@ class JapanCalendar(WesternCalendar):
             (WesternCalendar.get_nth_weekday_in_month(year, 7, MON, 3),
                 "Marine Day"),
 
+            (equinoxes[0], "Vernal Equinox Day"),
+
             (WesternCalendar.get_nth_weekday_in_month(year, 9, MON, 3),
                 "Respect-for-the-Aged Day"),
+
+            (equinoxes[1], "Autumnal Equinox Day"),
 
             (WesternCalendar.get_nth_weekday_in_month(year, 10, MON, 2),
                 "Health and Sports Day"),
         ]
 
         return days
+
+    def calculate_equinoxes(self, year):
+        """ calculate equinox with time zone """
+
+        import ephem
+        import pytz
+
+        tz = pytz.timezone('Asia/Tokyo')
+
+        d1 = ephem.next_equinox(str(year))
+        d = ephem.Date(str(d1))
+        equinox1 = d.datetime() + tz.utcoffset(d.datetime())
+
+        d2 = ephem.next_equinox(d1)
+        d = ephem.Date(str(d2))
+        equinox2 = d.datetime() + tz.utcoffset(d.datetime())
+
+        return (equinox1.date(), equinox2.date())
