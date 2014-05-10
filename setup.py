4c5065443eccfcb883724c75cc7b000c71c70ece
Jason R. Coombs <jaraco@jaraco.com>
2014-05-10 12:59:00 -0400

    Classifiers must be a list to be properly recognized as multi-value in distutils.upload.

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ if __name__ == '__main__':
         include_package_data=True,
         install_requires=REQUIREMENTS,
         zip_safe=False,
-        classifiers=(
+        classifiers=[
             'Development Status :: 4 - Beta',
             'Intended Audience :: Developers',
             'License :: OSI Approved :: MIT License',
@@ -55,5 +55,5 @@ if __name__ == '__main__':
             'Programming Language :: Python :: 2.7',
             'Programming Language :: Python :: 3',
             'Programming Language :: Python :: 3.3',
-        )
+        ],
     )
