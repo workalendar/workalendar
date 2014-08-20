a5c9da808fb38f87b805ce9f14773d72e7959c99
Random User <rnduser@posteo.de>
2014-08-20 16:59:17 +0200

    Use include_boxing_day instead of hardcoding

--- workalendar/europe.py
+++ workalendar/europe.py
@@ -362,13 +362,14 @@ class Germany(WesternCalendar, ChristianMixin):
     FIXED_HOLIDAYS = WesternCalendar.FIXED_HOLIDAYS + (
         (5, 1, "Labour Day"),
         (10, 3, "Day of German Unity"),
-        (12, 26, "Second Christmas Day"),
     )
 
     include_easter_monday = True
     include_ascension = True
     include_whit_monday = True
     include_good_friday = True
+    include_boxing_day = True
+    boxing_day_label = "Second Christmas Day"
 
 
 class BadenWurttemberg(Germany):
