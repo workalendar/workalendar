93909f7e76a36e96ac5d52c30652f0a885c0c673
Bruno Bord <bruno.bord@novapost.fr>
2014-08-25 09:06:38 +0200

    Saxony refactor, refs #91

--- workalendar/tests/test_europe.py
+++ workalendar/tests/test_europe.py
@@ -579,36 +579,18 @@ class SaarlandTest(GermanyTest):
         self.assertIn(date(2015, 11, 1), holidays)
 
 
-class SaxonyTest(GenericCalendarTest):
+class SaxonyTest(GermanyTest):
     cal_class = Saxony
 
-    def test_year_2014(self):
+    def test_extra_2014(self):
         holidays = self.cal.holidays_set(2014)
-        self.assertIn(date(2014, 1, 1), holidays)
-        self.assertIn(date(2014, 4, 18), holidays)
-        self.assertIn(date(2014, 4, 21), holidays)
-        self.assertIn(date(2014, 5, 1), holidays)
-        self.assertIn(date(2014, 5, 29), holidays)
-        self.assertIn(date(2014, 6, 9), holidays)
-        self.assertIn(date(2014, 10, 3), holidays)
-        self.assertIn(date(2014, 10, 31), holidays)
         self.assertIn(date(2014, 11, 19), holidays)
-        self.assertIn(date(2014, 12, 25), holidays)
-        self.assertIn(date(2014, 12, 26), holidays)
+        self.assertIn(date(2014, 10, 31), holidays)
 
-    def test_year_2015(self):
+    def test_extra_2015(self):
         holidays = self.cal.holidays_set(2015)
-        self.assertIn(date(2015, 1, 1), holidays)
-        self.assertIn(date(2015, 4, 3), holidays)
-        self.assertIn(date(2015, 4, 6), holidays)
-        self.assertIn(date(2015, 5, 1), holidays)
-        self.assertIn(date(2015, 5, 14), holidays)
-        self.assertIn(date(2015, 5, 25), holidays)
-        self.assertIn(date(2015, 10, 3), holidays)
-        self.assertIn(date(2015, 10, 31), holidays)
         self.assertIn(date(2015, 11, 18), holidays)
-        self.assertIn(date(2015, 12, 25), holidays)
-        self.assertIn(date(2015, 12, 26), holidays)
+        self.assertIn(date(2015, 10, 31), holidays)
 
 
 class SaxonyAnhaltTest(GenericCalendarTest):
