baa4f7bec12c425a112b46ae52b8dc681081cedb
Jan Malte Gerth <JanMalte@users.noreply.github.com>
2016-01-15 15:36:46 +0100

    added test case for is_holiday() datetime argument

--- workalendar/tests/test_core.py
+++ workalendar/tests/test_core.py
@@ -212,6 +212,8 @@ class MockCalendarTest(GenericCalendarTest):
     def test_datetime(self):
         self.assertFalse(
             self.cal.is_working_day(datetime(2014, 1, 1)))
+        self.assertTrue(
+            self.cal.is_holiday(datetime(2014, 1, 1)))
 
     def test_add_working_days_backwards(self):
         day = date(self.year, 1, 3)
