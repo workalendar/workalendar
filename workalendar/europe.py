20ddea3d57ad274d4b1512c821671334d97f2fea
Agustí Fita i Borrell <afita@gisce.net>
2016-01-18 10:27:30 +0100

    NEW Spain 2016 Holidays

--- workalendar/europe.py
+++ workalendar/europe.py
@@ -624,8 +624,10 @@ class Spain(WesternCalendar, ChristianMixin):
     include_immaculate_conception = True
     include_good_friday = True
     include_assumption = True
+    include_all_saints = True
 
     FIXED_HOLIDAYS = WesternCalendar.FIXED_HOLIDAYS + (
         (5, 1, u"Día del trabajador"),
-        (10, 12, u"Fiesta nacional de España")
+        (10, 12, u"Fiesta nacional de España"),
+        (12, 6, u"Día de la Constitución Española")
     )
