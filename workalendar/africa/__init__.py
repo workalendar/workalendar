f3819e3f751a48ca445d0a4dac120a572a1a1b34
Bruno Bord <bruno.bord@novapost.fr>
2016-06-29 12:34:53 +0200

    Africa module

---
+++ workalendar/africa/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from .algeria import Algeria
+from .benin import Benin
+from .ivory_coast import IvoryCoast
+from .madagascar import Madagascar
+from .sao_tome_principe import SaoTomeAndPrincipe
+from .south_africa import SouthAfrica
+
+__all__ = (
+    Algeria,
+    Benin,
+    IvoryCoast,
+    Madagascar,
+    SaoTomeAndPrincipe,
+    SouthAfrica,
+)
