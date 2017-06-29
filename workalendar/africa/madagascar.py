adf1c7f15d999ff30f193578413754a0773cb71d
Bruno Bord <bruno.bord@novapost.fr>
2017-06-29 17:02:56 +0200

    Splitted `africa.py` file into an `africa/` module

---
+++ workalendar/africa/madagascar.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+from workalendar.core import WesternCalendar, ChristianMixin
+
+
+class Madagascar(WesternCalendar, ChristianMixin):
+    "Madagascar"
+    FIXED_HOLIDAYS = WesternCalendar.FIXED_HOLIDAYS + (
+        (3, 29, "Martyrs' Day"),
+        (5, 1, "Labour Day"),
+        (6, 26, "Independence Day"),
+    )
+    include_easter_monday = True
+    include_ascension = True
+    include_whit_monday = True
+    include_assumption = True
+    include_all_saints = True
