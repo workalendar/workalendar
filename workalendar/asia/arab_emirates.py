15ce80ab6e1fd9b18786d716bcd9033dbeeaeaad
Alessandro Reichert <alessandro.reichert@ciopartner.com.br>
2018-06-27 15:23:15 -0300

    Update arab_emirates.py

--- workalendar/asia/arab_emirates.py
+++ workalendar/asia/arab_emirates.py
@@ -4,33 +4,27 @@ from __future__ import (absolute_import, division, print_function,
 
 from datetime import date
 
-from workalendar.core import WesternCalendar
+from workalendar.core import WesternCalendar, IslamicMixin
 from workalendar.core import MON, TUE, WED, FRI
 
 
-class ArabEmirates(WesternCalendar):
+class ArabEmirates(WesternCalendar, IslamicMixin):
     "Arab Emirates"
     FIXED_HOLIDAYS = WesternCalendar.FIXED_HOLIDAYS + (
         (4, 14, "Israa & Miaraj Night"),
-        (6, 14, "Eid Al Fitr Holiday"),
-        (6, 15, "Eid Al Fitr Holiday"),
-        (6, 16, "Eid Al Fitr Holiday"),
-        (6, 17, "Eid Al Fitr Holiday"),
         (8, 20, "Arafat (Haj) Day"),
-        (8, 21, "Eid Al Adha"),
-        (8, 22, "Eid Al Adha"),
-        (8, 23, "Eid Al Adha"),
         (9, 11, "Islamic New Year"),
         (11, 19, "Prophet Mohammed's Birthday"),
         (11, 30, "Martyrs' Day"),
         (12, 2, "UAE National Day"),
         (12, 3, "UAE National Day"),
     )
-    include_good_friday = False
-    include_easter_saturday = False
-    include_assumption = False
-    include_all_saints = False
-    include_immaculate_conception = False
+    # Islamic holidays
+    include_eid_al_fitr = True
+    length_eid_al_fitr = 4
+    include_eid_al_adha = True
+    length_eid_al_adha = 4
+    include_islamic_new_year = True
 
     def get_variable_days(self, year):
         days = super(ArabEmirates, self).get_variable_days(year)
