3d154bc25cfe087622238121bb42a1bfaff6a5c5
David Miguel <dvdmgl@gmail.com>
2018-08-09 23:43:42 +0100

    removed carnival and added restored holiday
    https://pt.wikipedia.org/wiki/Feriados_em_Portugal#Feriados_eliminados_em_2013%E2%80%932015
    
    Issue #275

--- workalendar/europe/portugal.py
+++ workalendar/europe/portugal.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from datetime import timedelta
+from datetime import date
 from workalendar.core import WesternCalendar, ChristianMixin
 from workalendar.registry import iso_register
 
@@ -11,6 +11,7 @@ class Portugal(WesternCalendar, ChristianMixin):
 
     include_good_friday = True
     include_easter_sunday = True
+    include_christmas = True
 
     FIXED_HOLIDAYS = WesternCalendar.FIXED_HOLIDAYS + (
         (4, 25, "Dia da Liberdade"),
@@ -20,11 +21,17 @@ class Portugal(WesternCalendar, ChristianMixin):
         (12, 8, "Imaculada Conceição"),
     )
 
-    def get_variable_entrudo(self, year):
-        easter_sunday = self.get_easter_sunday(year)
-        return easter_sunday - timedelta(days=47)
+    def get_fixed_holidays(self, year):
+        days = super(Portugal, self).get_fixed_holidays(year)
+        if year > 2015 or year < 2013:
+
+            days.append((date(year, 10, 5), "Implantação da República"))
+            days.append((date(year, 11, 1), "Todos os santos"))
+            days.append((date(year, 12, 1), "Restauração da Independência"))
+        return days
 
     def get_variable_days(self, year):
         days = super(Portugal, self).get_variable_days(year)
-        days.append((self.get_variable_entrudo(year), "Entrudo"))
+        if year > 2015 or year < 2013:
+            days.append((self.get_corpus_christi(year), "Corpus Christi"))
         return days
