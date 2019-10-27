315f940c616897d014308d049016767c1a622e16
Jason R. Coombs <jaraco@jaraco.com>
2019-10-27 11:30:24 -0400

    Move pyluach into a simple dependency.

--- setup.py
+++ setup.py
@@ -2,16 +2,5 @@
 
 import setuptools
 
-
 if __name__ == "__main__":
-    setuptools.setup(
-        use_scm_version=True,
-        requires=[
-            'pyluach',
-        ],
-        tests_require=[
-            'pandas',
-            'pytest-cov',
-            'pytest-flake8',
-        ],
-    )
+    setuptools.setup(use_scm_version=True)
