8cc83cae80e4f324b59a0758a0abc4b39304f6d2
Bruno Bord <bruno.bord@people-doc.com>
2019-11-03 12:05:36 +0100

    Added Python 3.8 classifier in setup.py

--- setup.py
+++ setup.py
@@ -55,6 +55,7 @@ params = dict(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
 )
 
