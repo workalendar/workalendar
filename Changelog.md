d9b7acb9f94c34b16c46a3d541eb5c4fabddd465
Bruno Bord <bruno.bord@people-doc.com>
2019-12-06 10:39:30 +0100

    Back to development => 7.3.0.dev0

--- Changelog.md
+++ Changelog.md
@@ -1,5 +1,9 @@
 # CHANGELOG
 
+## master (unreleased)
+
+Nothing here yet.
+
 ## v7.2.0 (2019-12-06)
 
 ### New calendars
--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ REQUIREMENTS = [
     'pyluach',
     'setuptools>=1.0',
 ]
-version = '7.2.0'
+version = '7.3.0.dev0'
 __VERSION__ = version
 
 params = dict(
