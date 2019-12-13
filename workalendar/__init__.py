df9f4bd996a41a3fdaf9947d8e27fc2973e6a54f
Rémy HUBSCHER <remy@chefclub.tv>
2019-12-13 16:15:36 +0100

    Embed gettext compiles files with the release.

--- workalendar/__init__.py
+++ workalendar/__init__.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 import pkg_resources
+import gettext
 
+# Load gettext locale files from the package wheel
+locale_path = pkg_resources.resource_filename('workalendar', 'locales')
+gettext.find('workalendar', localedir=locale_path, fallback=True)
 
 #: Module version, as defined in PEP-0396.
 __version__ = pkg_resources.get_distribution(__package__).version
