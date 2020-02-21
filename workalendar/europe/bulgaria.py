265fec9efa7b8be2d4a280ffb840fbbd7e3da0d7
Bruno Bord <bruno.bord@people-doc.com>
2020-02-21 11:32:07 +0100

    Added Bulgaria translations

--- workalendar/europe/bulgaria.py
+++ workalendar/europe/bulgaria.py
@@ -1,3 +1,5 @@
+from gettext import gettext as _
+
 from ..core import WesternCalendar, ChristianMixin
 from ..registry_tools import iso_register
 
@@ -7,14 +9,15 @@ class Bulgaria(WesternCalendar, ChristianMixin):
     'Bulgaria'
 
     FIXED_HOLIDAYS = WesternCalendar.FIXED_HOLIDAYS + (
-        (3, 3, "Liberation Day"),  # Ден на Освобождението на Б
-        (5, 1, "International Workers' Day"),  # Ден на труда и на междунар
-        (5, 6, "Saint George's Day"),  # Гергьовден, ден на храброс
-        (5, 24, "Saints Cyril & Methodius Day"),  # Ден на българската просвет
-        (9, 6, "Unification Day"),  # Ден на Съединението
-        (9, 22, "Independence Day"),  # Ден на независимостта на Б
+        (3, 3, _("Liberation Day")),  # Ден на Освобождението на Б
+        (5, 1, _("International Workers' Day")),  # Ден на труда и на междунар
+        (5, 6, _("Saint George's Day")),  # Гергьовден, ден на храброс
+        # Ден на българската просвет
+        (5, 24, _("Saints Cyril & Methodius Day")),
+        (9, 6, _("Unification Day")),  # Ден на Съединението
+        (9, 22, _("Independence Day")),  # Ден на независимостта на Б
         # wikipedia says Non-attendance day for schools, otherwise a working da
-        # (11, 1, "National Awakening Day"),  # Ден на народните будители
+        # (11, 1, _("National Awakening Day")),  # Ден на народните будители
 
     )
 
