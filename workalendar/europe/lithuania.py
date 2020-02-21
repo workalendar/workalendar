00b5ca8c3f35636a3c7f8beb7d5a393e8aff1b16
Bruno Bord <bruno.bord@people-doc.com>
2020-02-21 17:13:07 +0100

    Added Lithuania translations

--- workalendar/europe/lithuania.py
+++ workalendar/europe/lithuania.py
@@ -1,3 +1,5 @@
+from gettext import gettext as _
+
 from ..core import WesternCalendar, ChristianMixin, SUN
 from ..registry_tools import iso_register
 
@@ -7,13 +9,13 @@ class Lithuania(WesternCalendar, ChristianMixin):
     'Lithuania'
 
     FIXED_HOLIDAYS = WesternCalendar.FIXED_HOLIDAYS + (
-        (2, 16, "Restoration of the State Day"),
-        (3, 11, "Restoration of Independence Day"),
-        (5, 1, "Labour Day"),
-        (6, 24, "St. John's Day"),
-        (7, 6, "Anniversary of the Coronation of King Mindaugas"),
-        (8, 15, "Assumption Day"),
-        (11, 1, "All Saints' Day"),
+        (2, 16, _("Restoration of the State Day")),
+        (3, 11, _("Restoration of Independence Day")),
+        (5, 1, _("Labour Day")),
+        (6, 24, _("St. John's Day")),
+        (7, 6, _("Anniversary of the Coronation of King Mindaugas")),
+        (8, 15, _("Assumption Day")),
+        (11, 1, _("All Saints' Day")),
     )
 
     include_easter_sunday = True
@@ -21,18 +23,18 @@ class Lithuania(WesternCalendar, ChristianMixin):
     include_christmas_eve = True
     include_christmas = True
     include_boxing_day = True
-    boxing_day_label = "Second day of Christmas"
+    boxing_day_label = _("Second day of Christmas")
 
     def get_mothers_day(self, year):
         return (
             Lithuania.get_nth_weekday_in_month(year, 5, SUN, 1),
-            "Mother's day"
+            _("Mother's day")
         )
 
     def get_fathers_day(self, year):
         return (
             Lithuania.get_nth_weekday_in_month(year, 6, SUN, 1),
-            "Father's day"
+            _("Father's day")
         )
 
     def get_variable_days(self, year):
