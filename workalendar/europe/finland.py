4a582d23b4b1a6be16e23dbc59e048f38c9040ea
Bruno Bord <bruno.bord@people-doc.com>
2020-02-21 16:08:31 +0100

    Added Finland translations

--- workalendar/europe/finland.py
+++ workalendar/europe/finland.py
@@ -1,4 +1,6 @@
 from datetime import date
+from gettext import gettext as _
+
 from ..core import WesternCalendar, ChristianMixin, FRI, SAT
 from ..registry_tools import iso_register
 
@@ -13,14 +15,14 @@ class Finland(WesternCalendar, ChristianMixin):
     include_easter_monday = True
     include_ascension = True
     include_whit_sunday = True
-    whit_sunday_label = 'Pentecost'
+    whit_sunday_label = _('Pentecost')
     include_christmas_eve = True
     include_boxing_day = True
-    boxing_day_label = "St. Stephen's Day"
+    boxing_day_label = _("St. Stephen's Day")
 
     FIXED_HOLIDAYS = WesternCalendar.FIXED_HOLIDAYS + (
-        (5, 1, "Labour Day"),
-        (12, 6, "Independence Day"),
+        (5, 1, _("Labour Day")),
+        (12, 6, _("Independence Day")),
     )
 
     def get_midsummer_eve(self, year):
@@ -42,7 +44,7 @@ class Finland(WesternCalendar, ChristianMixin):
 
     def get_variable_days(self, year):
         days = super().get_variable_days(year)
-        days.append((self.get_midsummer_eve(year), "Midsummer's Eve"))
-        days.append((self.get_midsummer_day(year), "Midsummer's Day"))
-        days.append((self.get_variable_all_saints(year), "All Saints"))
+        days.append((self.get_midsummer_eve(year), _("Midsummer's Eve")))
+        days.append((self.get_midsummer_day(year), _("Midsummer's Day")))
+        days.append((self.get_variable_all_saints(year), _("All Saints")))
         return days
