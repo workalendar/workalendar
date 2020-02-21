cff0c7ec86b875dc46001d8e018de114c1a8a00a
Bruno Bord <bruno.bord@people-doc.com>
2020-02-21 17:44:13 +0100

    Added Slovakia translations

--- workalendar/europe/slovakia.py
+++ workalendar/europe/slovakia.py
@@ -1,3 +1,5 @@
+from gettext import gettext as _
+
 from ..core import WesternCalendar, ChristianMixin
 from ..registry_tools import iso_register
 
@@ -12,15 +14,15 @@ class Slovakia(WesternCalendar, ChristianMixin):
     include_all_saints = True
     include_christmas_eve = True
     include_boxing_day = True
-    boxing_day_label = "St. Stephen's Day (The Second Christmas Day)"
+    boxing_day_label = _("St. Stephen's Day (The Second Christmas Day)")
 
     FIXED_HOLIDAYS = WesternCalendar.FIXED_HOLIDAYS + (
-        (1, 1, "Day of the Establishment of the Slovak Republic"),
-        (5, 1, "Labour Day"),
-        (5, 8, "Liberation Day"),
-        (7, 5, "Saints Cyril and Methodius Day"),
-        (8, 29, "Slovak National Uprising anniversary"),
-        (9, 1, "Day of the Constitution of the Slovak Republic"),
-        (9, 15, "Day of Blessed Virgin Mary, patron saint of Slovakia"),
-        (11, 17, "Struggle for Freedom and Democracy Day"),
+        (1, 1, _("Day of the Establishment of the Slovak Republic")),
+        (5, 1, _("Labour Day")),
+        (5, 8, _("Liberation Day")),
+        (7, 5, _("Saints Cyril and Methodius Day")),
+        (8, 29, _("Slovak National Uprising anniversary")),
+        (9, 1, _("Day of the Constitution of the Slovak Republic")),
+        (9, 15, _("Day of Blessed Virgin Mary, patron saint of Slovakia")),
+        (11, 17, _("Struggle for Freedom and Democracy Day")),
     )
