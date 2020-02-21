91439757e9d91c2b9e39e30ca98affbfdae73b13
Bruno Bord <bruno.bord@people-doc.com>
2020-02-21 15:51:47 +0100

    Added Cyprus translations

--- workalendar/europe/cyprus.py
+++ workalendar/europe/cyprus.py
@@ -1,4 +1,6 @@
+from gettext import gettext as _
 from datetime import timedelta
+
 from ..core import WesternCalendar, ChristianMixin
 from ..registry_tools import iso_register
 
@@ -14,22 +16,22 @@ class Cyprus(WesternCalendar, ChristianMixin):
     include_easter_sunday = True
     include_easter_monday = True
     include_whit_monday = True
-    whit_monday_label = 'Pentecost Monday'
+    whit_monday_label = _('Pentecost Monday')
     include_christmas_eve = True
     include_christmas_day = True
     include_boxing_day = True
 
     FIXED_HOLIDAYS = WesternCalendar.FIXED_HOLIDAYS + (
-        (3, 25, "Greek Independence Day"),
-        (4, 1, "Cyprus National Day"),
-        (5, 1, "Labour Day"),
-        (7, 15, "Dormition of the Theotokos"),
-        (10, 1, "Cyprus Independence Day"),
-        (10, 28, "Greek National Day"),
+        (3, 25, _("Greek Independence Day")),
+        (4, 1, _("Cyprus National Day")),
+        (5, 1, _("Labour Day")),
+        (7, 15, _("Dormition of the Theotokos")),
+        (10, 1, _("Cyprus Independence Day")),
+        (10, 28, _("Greek National Day")),
     )
 
     def get_variable_days(self, year):
         days = super().get_variable_days(year)
         days.append((self.get_easter_monday(year) +
-                     timedelta(days=1), "Easter Tuesday"))
+                     timedelta(days=1), _("Easter Tuesday")))
         return days
