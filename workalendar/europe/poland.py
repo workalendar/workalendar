4fa093b0244eed4316657b11e834a8e74db0bf46
Bruno Bord <bruno.bord@people-doc.com>
2020-02-21 17:27:46 +0100

    Added Poland translations

--- workalendar/europe/poland.py
+++ workalendar/europe/poland.py
@@ -1,3 +1,5 @@
+from gettext import gettext as _
+
 from ..core import WesternCalendar, ChristianMixin
 from ..registry_tools import iso_register
 
@@ -7,15 +9,15 @@ class Poland(WesternCalendar, ChristianMixin):
     'Poland'
 
     FIXED_HOLIDAYS = WesternCalendar.FIXED_HOLIDAYS + (
-        (1, 6, 'Trzech Kroli'),
-        (5, 1, 'Labour Day'),
-        (5, 3, 'Constitution Day'),
-        (11, 11, 'Independence Day'),
+        (1, 6, _('Trzech Kroli')),
+        (5, 1, _('Labour Day')),
+        (5, 3, _('Constitution Day')),
+        (11, 11, _('Independence Day')),
     )
     include_easter_sunday = True
     include_easter_monday = True
     include_whit_sunday = True
-    whit_sunday_label = "Pentecost Sunday"
+    whit_sunday_label = _("Pentecost Sunday")
     include_corpus_christi = True
     include_assumption = True
     include_all_saints = True
