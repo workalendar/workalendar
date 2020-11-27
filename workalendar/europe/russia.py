22292c567837aad0be61c00c0c48eaa16b69dddd
Bruno Bord <bruno.bord@people-doc.com>
2020-11-27 18:07:04 +0100

    Fix Russia Holidays for the year 2021
    refs #578

--- workalendar/europe/russia.py
+++ workalendar/europe/russia.py
@@ -1,6 +1,6 @@
 from datetime import date
 
-from ..core import OrthodoxCalendar, MON, daterange
+from ..core import OrthodoxCalendar, MON, daterange, cleaned_date
 from ..registry_tools import iso_register
 
 
@@ -26,6 +26,12 @@ class Russia(OrthodoxCalendar):
     covid19_2020_end = date(2020, 4, 30)
 
     def get_fixed_holidays(self, year):
+
+        if year >= 1992:
+            self.labour_day_label = "The Day of Spring and Labour"
+        else:
+            self.labour_day_label = "International Workers' Day"
+
         days = super().get_fixed_holidays(year)
 
         if year >= 2005:
@@ -61,6 +67,12 @@ class Russia(OrthodoxCalendar):
 
             # Constitution Vote Public Holiday
             days.append((date(year, 7, 1), "Constitution Vote Public Holiday"))
+        elif year == 2021:
+            days.extend([
+                (date(year, 2, 22), "Day Before Defendence of the Fatherland"),
+                (date(year, 11, 5), 'Day After Day of Unity'),
+                (date(year, 12, 31), "New Year's Eve"),
+            ])
         return days
 
     def get_calendar_holidays(self, year):
@@ -79,3 +91,14 @@ class Russia(OrthodoxCalendar):
                 ))
         holidays.extend(shifts)
         return holidays
+
+    def is_working_day(self, day,
+                       extra_working_days=None, extra_holidays=None):
+        day = cleaned_date(day)
+        if day == date(2021, 2, 20):
+            return True
+        return super().is_working_day(
+            day,
+            extra_working_days=extra_working_days,
+            extra_holidays=extra_holidays
+        )
--- workalendar/tests/test_europe.py
+++ workalendar/tests/test_europe.py
@@ -1,4 +1,4 @@
-from datetime import date, timedelta
+from datetime import date, timedelta, datetime
 from collections import Counter
 
 from . import GenericCalendarTest
@@ -1517,6 +1517,39 @@ class RussiaTest(GenericCalendarTest):
         # This calendar is an Orthodox calendar & it doesn't include Dec 25th
         holidays = self.cal.holidays_set(2020)
         self.assertNotIn(date(2020, 12, 25), holidays)
+
+    def test_holidays_2021(self):
+        holidays = self.cal.holidays(2021)
+        holidays_2021 = [
+            (date(2021, 1, 1), "New year"),
+            (date(2021, 1, 2), "Day After New Year"),  # SAT
+            (date(2021, 1, 3), "Third Day after New Year"),  # SUN
+            (date(2021, 1, 4), "Fourth Day after New Year"),
+            (date(2021, 1, 5), "Fifth Day after New Year"),
+            (date(2021, 1, 6), "Sixth Day after New Year"),
+            (date(2021, 1, 7), "Christmas"),
+            (date(2021, 1, 8),  "Eighth Day after New Year"),
+            (date(2021, 2, 22), "Day Before Defendence of the Fatherland"),
+            (date(2021, 2, 23), "Defendence of the Fatherland"),
+            (date(2021, 3, 8), "International Women's Day"),
+            (date(2021, 5, 1), "The Day of Spring and Labour"),
+            (date(2021, 5, 3), "The Day of Spring and Labour shift"),
+            (date(2021, 5, 9), "Victory Day"),
+            (date(2021, 5, 10), "Victory Day shift"),
+            (date(2021, 6, 12), "National Day"),
+            (date(2021, 6, 14), "National Day shift"),
+            (date(2021, 11, 4), "Day of Unity"),
+            (date(2021, 11, 5), "Day After Day of Unity"),
+            (date(2021, 12, 31), "New Year's Eve")
+        ]
+        self.assertEqual(holidays, holidays_2021)
+
+    def test_year_2021_extra_working_day(self):
+        self.assertTrue(self.cal.is_working_day(date(2021, 2, 20)))
+        self.assertTrue(self.cal.is_working_day(datetime(2021, 2, 20)))
+
+        self.assertTrue(self.cal.is_working_day(date(2021, 2, 19)))
+        self.assertTrue(self.cal.is_working_day(datetime(2020, 2, 19)))
 
 
 class UkraineTest(GenericCalendarTest):
