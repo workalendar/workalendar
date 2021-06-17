39c0125958873ad3f95a6a240dadce202094f3fd
Joachim Jablon <ewjoachim@gmail.com>
2021-06-17 18:06:31 +0200

    Replace pkg_resources with importlib(_|.)metadata

--- Changelog.md
+++ Changelog.md
@@ -3,6 +3,7 @@
 ## master (unreleased)
 
 - The Workalendar project has been moved from Peopledoc's organization to its own (#651).
+- Replace `pkg_resources` with `importlib_metadata` to fetch the version number in `__init__.py` (#657)
 
 ## v15.3.0 (2021-05-07)
 
--- workalendar/__init__.py
+++ workalendar/__init__.py
@@ -1,5 +1,12 @@
-import pkg_resources
+import sys
 
+if sys.version_info[:2] < (3, 8):  # coverage: exclude
+    import importlib_metadata
+else:  # coverage: exclude
+    import importlib.metadata as importlib_metadata
 
-#: Module version, as defined in PEP-0396.
-__version__ = pkg_resources.get_distribution(__package__).version
+try:
+    #: Module version, as defined in PEP-0396.
+    __version__ = importlib_metadata.version(__name__)
+except importlib_metadata.PackageNotFoundError:  # pragma: no cover
+    __version__ = "unknown"
