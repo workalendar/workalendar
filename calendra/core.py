9e3ebe4406a8b87e3a54ef9c0543fd998c400ee5
Jason R. Coombs <jaraco@jaraco.com>
2022-09-01 21:16:40 -0400

    Move holiday implementation to its own module to simplify merging.

--- calendra/core.py
+++ calendra/core.py
@@ -5,7 +5,6 @@ import os
 from os.path import isdir
 import warnings
 
-import itertools
 from calendar import monthrange
 from datetime import date, timedelta, datetime
 from typing import Optional, List
@@ -14,13 +13,13 @@ from calverter import Calverter
 from dateutil import easter
 from lunardate import LunarDate
 from dateutil import relativedelta as rd
-from more_itertools import recipes
 
 from .exceptions import (
     UnsupportedDateType, CalendarError,
     ICalExportRangeError, ICalExportTargetPathError
 )
 from . import __version__
+from .holiday import Holiday
 
 MON, TUE, WED, THU, FRI, SAT, SUN = range(7)
 
@@ -65,101 +64,6 @@ def daterange(start, end):
     while day <= end:
         yield day
         day += timedelta(days=1)
-
-
-class Holiday(date):
-    """
-    A named holiday with an indicated date, name, and additional keyword
-    attributes.
-
-    >>> nyd = Holiday(date(2014, 1, 1), "New year")
-
-    But if New Year's Eve is also a holiday, and it too falls on a weekend,
-    many calendars will have that holiday fall back to the previous friday:
-
-    >>> nye = Holiday(date(2014, 12, 31), "New year's eve",
-    ...     observance_shift=dict(weekday=rd.FR(-1)))
-
-    For compatibility, a Holiday may be treated like a tuple of (date, label)
-
-    >>> nyd[0] == date(2014, 1, 1)
-    True
-    >>> nyd[1]
-    'New year'
-    >>> d, label = nyd
-    """
-
-    def __new__(cls, date, *args, **kwargs):
-        return super().__new__(
-            cls, date.year, date.month, date.day)
-
-    def __init__(self, date, name='Holiday', **kwargs):
-        self.name = name
-        vars(self).update(kwargs)
-
-    def __getitem__(self, n):
-        """
-        for compatibility as a two-tuple
-        """
-        tp = self, self.name
-        return tp[n]
-
-    def __iter__(self):
-        """
-        for compatibility as a two-tuple
-        """
-        tp = self, self.name
-        return iter(tp)
-
-    def replace(self, *args, **kwargs):
-        replaced = super().replace(*args, **kwargs)
-        vars(replaced).update(vars(self))
-        return replaced
-
-    def __add__(self, other):
-        orig = date(self.year, self.month, self.day)
-        return Holiday(orig + other, **vars(self))
-
-    def __sub__(self, other):
-        orig = date(self.year, self.month, self.day)
-        return Holiday(orig - other, **vars(self))
-
-    def nearest_weekday(self, calendar):
-        """
-        Return the nearest weekday to self.
-        """
-        weekend_days = calendar.get_weekend_days()
-        deltas = (timedelta(n) for n in itertools.count())
-        candidates = recipes.flatten(
-            (self - delta, self + delta)
-            for delta in deltas
-        )
-        matches = (
-            day for day in candidates
-            if day.weekday() not in weekend_days
-        )
-        return next(matches)
-
-    @classmethod
-    def _from_fixed_definition(cls, item):
-        """For backward compatibility, load Holiday object from an item of
-        FIXED_HOLIDAYS class property, which might be just a tuple of
-        month, day, label.
-        """
-        if isinstance(item, tuple):
-            month, day, label = item
-            any_year = 2000
-            item = Holiday(date(any_year, month, day), label)
-        return item
-
-    @classmethod
-    def _from_resolved_definition(cls, item):
-        """For backward compatibility, load Holiday object from a two-tuple
-        or existing Holiday instance.
-        """
-        if isinstance(item, tuple):
-            item = Holiday(*item)
-        return item
 
 
 class CoreCalendar:
---
+++ calendra/holiday.py
@@ -0,0 +1,99 @@
+import itertools
+from datetime import date, timedelta
+from more_itertools import recipes
+
+
+class Holiday(date):
+    """
+    A named holiday with an indicated date, name, and additional keyword
+    attributes.
+
+    >>> nyd = Holiday(date(2014, 1, 1), "New year")
+
+    But if New Year's Eve is also a holiday, and it too falls on a weekend,
+    many calendars will have that holiday fall back to the previous friday:
+
+    >>> from dateutil import relativedelta as rd
+    >>> nye = Holiday(date(2014, 12, 31), "New year's eve",
+    ...     observance_shift=dict(weekday=rd.FR(-1)))
+
+    For compatibility, a Holiday may be treated like a tuple of (date, label)
+
+    >>> nyd[0] == date(2014, 1, 1)
+    True
+    >>> nyd[1]
+    'New year'
+    >>> d, label = nyd
+    """
+
+    def __new__(cls, date, *args, **kwargs):
+        return super().__new__(
+            cls, date.year, date.month, date.day)
+
+    def __init__(self, date, name='Holiday', **kwargs):
+        self.name = name
+        vars(self).update(kwargs)
+
+    def __getitem__(self, n):
+        """
+        for compatibility as a two-tuple
+        """
+        tp = self, self.name
+        return tp[n]
+
+    def __iter__(self):
+        """
+        for compatibility as a two-tuple
+        """
+        tp = self, self.name
+        return iter(tp)
+
+    def replace(self, *args, **kwargs):
+        replaced = super().replace(*args, **kwargs)
+        vars(replaced).update(vars(self))
+        return replaced
+
+    def __add__(self, other):
+        orig = date(self.year, self.month, self.day)
+        return Holiday(orig + other, **vars(self))
+
+    def __sub__(self, other):
+        orig = date(self.year, self.month, self.day)
+        return Holiday(orig - other, **vars(self))
+
+    def nearest_weekday(self, calendar):
+        """
+        Return the nearest weekday to self.
+        """
+        weekend_days = calendar.get_weekend_days()
+        deltas = (timedelta(n) for n in itertools.count())
+        candidates = recipes.flatten(
+            (self - delta, self + delta)
+            for delta in deltas
+        )
+        matches = (
+            day for day in candidates
+            if day.weekday() not in weekend_days
+        )
+        return next(matches)
+
+    @classmethod
+    def _from_fixed_definition(cls, item):
+        """For backward compatibility, load Holiday object from an item of
+        FIXED_HOLIDAYS class property, which might be just a tuple of
+        month, day, label.
+        """
+        if isinstance(item, tuple):
+            month, day, label = item
+            any_year = 2000
+            item = Holiday(date(any_year, month, day), label)
+        return item
+
+    @classmethod
+    def _from_resolved_definition(cls, item):
+        """For backward compatibility, load Holiday object from a two-tuple
+        or existing Holiday instance.
+        """
+        if isinstance(item, tuple):
+            item = Holiday(*item)
+        return item
